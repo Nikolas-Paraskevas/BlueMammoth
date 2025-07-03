fe9017bf787efd40ea8f93eaa998d506774db4c1
Nikolas-Paraskevas <Frzx43@gmail.com>
2025-07-03 10:56:23 +0200

    almost done
    Co-Authored-By: Fatine <596522@edu.rocmn.nl>

--- Assets/Bullet.cs
+++ Assets/Bullet.cs
@@ -7,7 +7,6 @@ public class Bullet : MonoBehaviour
 
     private void Start()
     {
-        Destroy(gameObject, 5f); // Destroy bullet after 5 seconds to prevent clutter
     }
 
     private void Update()
@@ -17,6 +16,7 @@ public class Bullet : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+        Destroy(gameObject); // Destroy bullet after 5 seconds to prevent clutter
         if (collision.gameObject.CompareTag("Target"))
         {
             EnemyTarget enemy = collision.gameObject.GetComponent<EnemyTarget>();
